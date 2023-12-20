81c2addffd5fdd023ee5fde57f9f62d6ee8a7cd8
Neil Burningham <neil.burningham@mediakind.com>
2023-12-20 21:44:30 +0000

    Code up is_adjacent
    - first test passes
    - but the long thing gives the wrong answer...!

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -6,6 +6,31 @@ struct Number {
     right_col: usize,
 }
 
+impl Number {
+    fn is_adjacent(&self, symbols: &Vec<Symbol>) -> bool {
+        // 0:     0  1  2  3  4
+        // 1:     0  1a 2a 3  4
+        // 2:     0  1  2  3  4
+        for s in symbols {
+            let row_start = if self.row > 0 { self.row - 1 } else { self.row };
+            let col_start = if self.left_col > 0 {
+                self.left_col - 1
+            } else {
+                self.left_col
+            };
+            if s.row >= row_start
+                && s.row <= self.row + 1
+                && s.col >= col_start
+                && s.col <= self.right_col + 1
+            {
+                // print!("NAB: Decided that {:?} is adjacent to {:?}!\n", self, s);
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
 #[derive(Debug)]
 struct Symbol {
     char: char,
@@ -13,19 +38,15 @@ struct Symbol {
     col: usize,
 }
 
-fn process(data: &str) {
+fn process(data: &str) -> u32 {
     let mut numbers: Vec<Number> = vec![];
     let mut symbols: Vec<Symbol> = vec![];
 
     for (line_index, line) in data.lines().enumerate() {
         let mut start_col = 0;
         let mut num_chars = String::new();
-        // print!("NAB: found a lovely line at {}! {}\n", line_index, line);
+
         for (char_index, char) in line.chars().enumerate() {
-            // print!(
-            //     "NAB: found a lovely character at {}! {}\n",
-            //     char_index, char
-            // );
             if char.is_numeric() {
                 if num_chars.is_empty() {
                     start_col = char_index;
@@ -54,18 +75,50 @@ fn process(data: &str) {
         }
     }
 
-    print!("NAB: symbols are: {:?}", symbols);
-    print!("NAB: numbers are: {:?}", numbers);
+    // print!("NAB: symbols are: {:?}", symbols);
+    // print!("NAB: numbers are: {:?}", numbers);
+
+    let mut sum = 0;
+
+    for num in numbers {
+        if num.is_adjacent(&symbols) {
+            sum += num.val;
+        }
+    }
+
+    sum
 }
 
 fn main() {
-    let data = std::fs::read_to_string("data/day3_test.txt").unwrap();
+    // let data = std::fs::read_to_string("data/day3_test.txt").unwrap();
 
-    process(&data);
+    // let sum = process(&data);
+    // print!("Sum is {}\n", sum);
+
+    let data = std::fs::read_to_string("data/day3.txt").unwrap();
+
+    let sum = process(&data);
+    print!("Sum is {}\n", sum);
 }
 
 #[cfg(test)]
 mod test {
+    use super::*;
+
     #[test]
-    fn testicle() {}
+    fn testicle() {
+        let data = std::fs::read_to_string("data/day3_test.txt").unwrap();
+
+        let sum = process(&data);
+        assert_eq!(sum, 4361);
+    }
+
+    #[test]
+    fn test_larger() {
+        let data = std::fs::read_to_string("data/day3.txt").unwrap();
+
+        let sum = process(&data);
+        assert_ne!(sum, 531318);
+        // TODO! This is the wrong answer...!
+    }
 }
